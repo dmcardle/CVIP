dc31922384d2105deabbf3d2ebf2afc916117dfe
Dan McArdle <zingermc@gmail.com>
2014-11-17 17:57:09 -0500

    improved quality of debug script

--- homework3/BlockMatchDebug.m
+++ homework3/BlockMatchDebug.m
@@ -3,11 +3,11 @@ n = 100;
 m = 50;
 
 % create im1
-useim1 = zeros(n);
+useim1 = rand(n);
 x = rand(50);
 useim1(5:5+m-1, 5:5+m-1) = x;
 % create im2 (using same random block)
-useim2 = zeros(n);
+useim2 = circshift(useim1, 20);
 useim2(5:5+m-1, 23:23+m-1) = x;
 
 temp = useim1;
@@ -16,7 +16,7 @@ useim2 = temp;
 
 
 bDraw = false;
-blocksize = 10;
+blocksize = 20;
 [disparityMap, err] = BlockMatch(useim2, useim1, 0, blocksize, bDraw);
 
 imshow(disparityMap, [])
